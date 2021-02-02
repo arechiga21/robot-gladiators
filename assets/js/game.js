ae7e705d4fac2a7de3bb658482ff0fc117f25db3
arechiga2 <arechiga21jj@gmail.com>
2021-02-02 14:51:35 -0700

    add console log to JavaScript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("what is your robot's name?");
+
+console.log("This logs a string, good for leaving yourself a message")
+
+console.log(playerName)
+
+console.log(10+10);
+
+console.log("Our robot's name is " + playerName)
+
+//fight();
